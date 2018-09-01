f7ffbb0e9bfb738322329dd04c09cd1b514cb56d
stibbs1998 <sj28tibb@siena.edu>
2018-09-01 14:45:02 -0400

    sorted teams

--- scripts/market_rating.py
+++ scripts/market_rating.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pylab as plt
 
+
+def sort_list(l1, l2):
+        zipped_pairs = zip(l1,l2)
+        z = [x for _, x in sorted(zipped_pairs)]
+        return z
+
+
 data = np.loadtxt('../RSN_Ratings_by_Game.csv',delimiter = ',', dtype = str)
 teams, tvs  = np.loadtxt('../DMA_Households.csv',delimiter = ',', dtype = str, unpack = True, skiprows = 1)
 
@@ -35,24 +42,28 @@ for season in ['2014-15','2015-16','2016-17','2017-18']:
 			avg = np.nanmean(all_games)
 			ratings.append(avg)
 	
+	team_ticks = sort_list(ratings,list(teams))
+	sorted_tvs = sort_list(ratings,tvs)
+	ratings.sort()
+	avg_view = np.array(sorted_tvs)*10**4 * np.array(ratings) 
 	colors = np.arange(29)
 	xticks = np.arange(29)
 	plt.figure(figsize = (12,6))
-	plt.scatter(xticks,ratings,s=5*np.pi*tvs**2,c=colors)
+	plt.scatter(xticks,ratings,s=5*np.pi*np.array(sorted_tvs)**2,c=colors)
 	if type(past) != type(None):
-		plt.scatter(xticks,past,c='r',s=0.75,label = 'Previous Season')
-	plt.xticks(np.arange(29),list(teams),rotation=75)
+		plt.scatter(xticks,past,c='r',s=5,label = 'Previous Season')
+	plt.xticks(np.arange(29),team_ticks,rotation=75)
 	plt.ylabel('Local TV Rating')
 	plt.title('TV Rating vs. Size of Market in %s'%season)
 	plt.legend()
 	plt.savefig('../plots/avg_rating%s.png'%season)
 	plt.show()
 	
-	avg_view = tvs*10**4 * ratings 
-	
+
+
 	plt.figure(figsize = (12,6))
-	plt.scatter(xticks,avg_view,s=5*np.pi*tvs**2,c=colors)
-	plt.xticks(np.arange(29),list(teams),rotation = 75)
+	plt.scatter(xticks,avg_view,s=5*np.pi*np.array(sorted_tvs)**2,c=colors)
+	plt.xticks(np.arange(29),team_ticks,rotation = 75)
 	plt.ylabel('Avg Viewers Per Game')
 	plt.title('Avg Local Viewers Per Game in %s' %season)
 	plt.ylim(0, 2.5*10**5)
