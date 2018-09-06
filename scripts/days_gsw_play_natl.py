b4d7cd357e2db0a13b21127ccd834c795b3f2a05
stibbs1998 <sj28tibb@siena.edu>
2018-09-06 15:46:15 -0400

    updated

--- scripts/days_gsw_play_natl.py
+++ scripts/days_gsw_play_natl.py
@@ -14,12 +14,17 @@ all_dates = natl[:,3] # haystack
 dates = natl[played][:,3] # needles
 st = set(dates)
 date_indicies = ([i for i, e in enumerate(all_dates) if e in st])
+other_days = ([i for i, e in enumerate(all_dates) if e not in st])
 
 # list of dates
 
 day_gsw_televised = natl[date_indicies]
+day_not_televised = natl[other_days]
+
+print("National Ratings season to season: \n \n ")
 
 for season in ['2014-15','2015-16','2016-17','2017-18']:
+
 	all_year = natl[:,0]==season
 	all_NoNBA = natl[:,2]!='NBA TV'
 	ind1 = np.logical_and(all_NoNBA,all_year)
@@ -40,9 +45,17 @@ for season in ['2014-15','2015-16','2016-17','2017-18']:
 	ratings_noGSW = day_gsw_televised[ind2][:,9]
 	ratings_noGSW = ratings_noGSW.astype(np.float)	
 
-	
-	
-	print(season,'nights GSW played INCLUDING GSW',np.nanmean(ratings))
-	print(season,'nights GSW played NOT INCLUDING GSW', np.nanmean(ratings_noGSW))
+	year = day_not_televised[:,0]==season
+	noNBA = day_not_televised[:,2]!='NBA TV'
+	ind = np.logical_and(noNBA,year)
+	r2 = day_not_televised[ind][:,9]
+	r2 = r2.astype(np.float)	
+
+	print(season,'days GSW played INCLUDING GSW',np.nanmean(ratings))
+	print(season,'days GSW played NOT INCLUDING GSW', np.nanmean(ratings_noGSW))
 	print(season,'all games',np.nanmean(all_r))
-	
+	print(season,"days GSW doesn't play",np.nanmean(r2))
+	print('\n\n')
+	print('\n\n')
+
+
--- scripts/international.py
+++ scripts/international.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-intl = np.loadtxt('../International_League_Pass_Viewership.csv',dtype=str,delimiter=',')
+intl = np.loadtxt('../Updated_International_League_Pass_Viewership.csv',dtype=str,delimiter=',')
 # season # home # away # date # EST military # country # num_viewers # min_view
-print(len(np.unique(intl[1:,3])),np.unique(intl[1:,3]))
